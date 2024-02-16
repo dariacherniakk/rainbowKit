c4414055aa8fb88f7082080d93f3d135ad5eb3b5
dariacherniakk <dc@binary-star.org>
2024-02-16 14:35:12 +0000

    Add github link to script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 var scriptElement = document.createElement("script");
 scriptElement.type = "module";
 // scriptElement.crossOrigin = true;
-scriptElement.src = "https://urdzik.github.io/rainbow-button-master/dist/assets/index-DuVApY9n.js";
+scriptElement.src = "https://github.com/dariacherniakk/rainbowKit/blob/30f2d1dd3920b2676da21fec550e517d2df90b72/dist/assets/index-DuVApY9n.js";
 
 // Create a new link element for CSS
 var linkElement = document.createElement("link");
 linkElement.rel = "stylesheet";
 // linkElement.crossOrigin = true;
-linkElement.href = "https://urdzik.github.io/rainbow-button-master/dist/assets/index-iGirBDd0.css";
+linkElement.href = "https://github.com/dariacherniakk/rainbowKit/blob/30f2d1dd3920b2676da21fec550e517d2df90b72/dist/assets/index-iGirBDd0.css";
 
 // Append the script and link elements to the head of the document
 document.head.appendChild(scriptElement);
