d7b40611ad435ec7710bcb1cf60661da77b78ee2
dariacherniakk <dc@binary-star.org>
2024-02-16 15:46:43 +0000

    Modify script.js

---
+++ dist/script.js
@@ -0,0 +1,15 @@
+// Create a new script element
+var scriptElement = document.createElement("script");
+scriptElement.type = "module";
+// scriptElement.crossOrigin = true;
+scriptElement.src = "https://dariacherniakk.github.io/rainbowKit/dist/assets/index-D2XvaBqC.js";
+
+// Create a new link element for CSS
+var linkElement = document.createElement("link");
+linkElement.rel = "stylesheet";
+// linkElement.crossOrigin = true;
+linkElement.href = "https://dariacherniakk.github.io/rainbowKit/dist/assets/index-iGirBDd0.css";
+
+// Append the script and link elements to the head of the document
+document.head.appendChild(scriptElement);
+document.head.appendChild(linkElement);
