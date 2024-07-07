a299efe4690ec4cd02b56f16b00dfa0d4de532dc
dariacherniakk <dc@binary-star.org>
2024-07-07 18:29:02 +0200

    MAke button roundness smaller

--- dist/assets/index-CO71lfdW.js
+++ dist/assets/index-CO71lfdW.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-BM-vUzBh.js";import{j as Ur}from"./index-DupJ1iX6.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-Btm23Sox.js";import{j as Ur}from"./index-DQT_xuLh.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
